2c6e1625a11cf41a7557d7b170545703515fcd21
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2022-05-31 18:20:01 +0530

    app2 final commit

--- expenseplanner/lib/main.dart
+++ expenseplanner/lib/main.dart
@@ -1,19 +1,119 @@
 // ignore_for_file: prefer_const_constructors,
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:expenseplanner/models/transaction.dart';
 import 'package:expenseplanner/new_transaction.dart';
-import 'package:flutter/material.dart';
 import 'package:expenseplanner/transactionlist.dart';
-import 'chart.dart';
+import 'package:expenseplanner/chart.dart';
 
-void main() => runApp(MyHomePage());
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
+  runApp(MyHomePage());
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Transaction> _userTransactions = [];
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Quicksand',
+        primarySwatch: Colors.purple,
+        accentColor: Colors.yellow,
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Transaction> _userTransactions = [
+    Transaction(
+      id: DateTime.now().toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 1)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 2)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 3)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 4)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 5)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 6)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 7)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: DateTime.now().add(Duration(hours: 8)).toString(),
+      title: 'test1',
+      amount: 100,
+      date: DateTime.now(),
+    ),
+  ];
+
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
@@ -58,63 +158,75 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Quicksand',
-        primarySwatch: Colors.purple,
-        accentColor: Colors.yellow,
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
-                fontFamily: 'OpenSans',
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 15,
-              ),
-            ),
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(
-            fontFamily: 'OpenSans',
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontSize: 20,
-          ),
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+
+    final appBar = AppBar(
+        title: Text(
+          'Personal Expenses',
+          // style: TextStyle(fontWeight: FontWeight.bold),
         ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Personal Expenses',
-            // style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          actions: <Widget>[
-            Builder(
-              builder: (context) => IconButton(
-                onPressed: () => _startAddNewTransaction(context),
-                icon: Icon(Icons.add),
-              ),
-            )
-          ],
-        ),
-        body: Column(
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add),
+          )
+        ]);
+
+    final txListwidget = Container(
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            0.7,
+        child: TransactionList(_userTransactions, _deleteTransaction));
+
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
           //mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Chart(_recentTransactions),
-            ),
-            Expanded(
-                child: TransactionList(_userTransactions, _deleteTransaction)),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Show Chart'),
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).accentColor,
+                    value: _showChart,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                  height: (mediaQuery.size.height -
+                          appBar.preferredSize.height -
+                          mediaQuery.padding.top) *
+                      0.3,
+                  child: Chart(_recentTransactions)),
+            if (!isLandscape) txListwidget,
+            if (isLandscape)
+              _showChart
+                  ? Container(
+                      height: (mediaQuery.size.height -
+                              appBar.preferredSize.height -
+                              mediaQuery.padding.top) *
+                          0.7,
+                      child: Chart(_recentTransactions))
+                  : txListwidget,
           ],
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: Builder(
-          builder: (context) => FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          ),
-        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- expenseplanner/lib/new_transaction.dart
+++ expenseplanner/lib/new_transaction.dart
@@ -49,49 +49,56 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-            ),
-            TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
-                keyboardType: TextInputType.number,
-                controller: _amountController,
-                onSubmitted: (_) => _submitData()),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Text(
-                      _selectedDate == null
-                          ? 'No Date Chosen!'
-                          : 'Date: ${DateFormat.yMMMd().format(_selectedDate!)}',
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: _presentDatePicker,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    textColor: Theme.of(context).primaryColor,
-                  ),
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
               ),
-            ),
-            ElevatedButton(
-              onPressed: _submitData,
-              child: Text('Add Transaction'),
-            ),
-          ],
+              TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  onSubmitted: (_) => _submitData()),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Date: ${DateFormat.yMMMd().format(_selectedDate!)}',
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: _presentDatePicker,
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _submitData,
+                child: Text('Add Transaction'),
+              ),
+            ],
+          ),
         ),
       ),
     );
